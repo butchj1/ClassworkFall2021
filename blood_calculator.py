f413b2ccf11d7c83ea838cfe803ab4db14df6b1e
Jimmy Butch <james.butch@duke.edu>
2021-09-01 12:36:33 -0400

    Addition of database.py to run blood analysis functions; New blood_calculator script that now runs both LDL and HDL calculators.

--- blood_calculator.py
+++ blood_calculator.py
@@ -1,40 +1,67 @@
-# Blood Calculator
+# HDL Blood Calculator
 
 def interface():
     print("Blood Calculator")
     keep_running = True
     while keep_running:
         print("Make a choice")
+        print("1 - HDL")
+        print("2 - LDL")
         print("9 - Quit")
         choice = int(input("Make a choice: "))
         if choice == 9:
             keep_running = False
         elif choice == 1:
-            HDL_Driver()
-            
-            
+            HDL_Driver() 
+        elif choice == 2:
+            LDL_Driver
     print(choice)
     return(choice)
 
-
-
 def HDL_Driver():
     HDL_value = hdl_input()
-    hdl_analysis(HDL_Value)
-    hdl_output(hdl_value, hdl_character)
-
+    HDL_character = HDL_analysis(HDL_value)
+    hdl_output(HDL_value, HDL_character)
+    
 def hdl_input():
     hdl_value = int(input(("Enter HDL Value: ")))
     return hdl_value
 
-def hdl_analysis():
-    if hdl_value >= 60:
+def HDL_analysis(HDL_value):
+    if HDL_value >= 60:
         return "Normal"
-    elif 40 < hdl_value < 60:
+    elif 40 <= HDL_value < 60:
         return "Borderline Low"
-    else hdl_value < 40:
+    else:
         return "Low"
         
-def hdl_output(hdl_value, hdl_answer):
+def hdl_output(HDL_value, HDL_answer):
+    print("The HDL value of {} is considered {}".format(HDL_value, HDL_answer))
+    return   
+
+#########################
+def LDL_Driver():
+    LDL_value = ldl_input()
+    LDL_character = LDL_analysis(LDL_value)
+    ldl_output(LDL_value, LDL_character)
     
-interface()
+def ldl_input():
+    ldl_value = int(input(("Enter LDL Value: ")))
+    return ldl_value
+
+def LDL_analysis(LDL_value):
+    if LDL_value < 130:
+        return "Normal"
+    elif 130 <= LDL_value < 159:
+        return "Borderline High"
+    elif 160 <= LDL_value < 189:
+        return "High"
+    else:
+        return "Very High"
+        
+def ldl_output(LDL_value, LDL_answer):
+    print("The LDL value of {} is considered {}".format(LDL_value, LDL_answer))
+    return   
+    
+if __name__ == "__main__":
+    interface()
